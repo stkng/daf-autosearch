1eb4c9dee8ffc4bc75e9021d72ad1d74a5d7197a
Steffen <22908998+stkng@users.noreply.github.com>
2023-02-26 10:03:13 +0100

    Update autosearch.js

--- autosearch.js
+++ autosearch.js
@@ -1,100 +1,110 @@
-const prefixInputLabel = document.createElement('label');
-prefixInputLabel.innerHTML = 'Pr&auml;fix';
-const prefixInput = document.createElement('input');
-prefixInput.value = '0G';
+let searchInterval;
+let searchInProgress = false;
+
+const make = (tag, content, value, type, id) => {
+  const el = document.createElement(tag);
+  content ? el.innerHTML = content : false;
+  value ? el.value = value : false;
+  id ? el.id = id : false;
+  type ? el.type = type : false;
+  return el;
+}
+
+const prefixInputLabel = make('label', 'Pr&auml;fix');
+const prefixInput = make('input', null, '0G');
 prefixInputLabel.appendChild(prefixInput);
-prefixInputLabel.style.display = 'flex';
-prefixInputLabel.style.flexDirection = 'column';
 
-const idInputLabel = document.createElement('label');
-idInputLabel.innerText = 'ID';
-const idInput = document.createElement('input');
-idInput.value = localStorage.getItem('last_id') || '';
+const idInputLabel = make('label', 'ID');
+const idInput = make('input', null, localStorage.getItem('last_id') || '');
 idInputLabel.appendChild(idInput);
-idInputLabel.style.display = 'flex';
-idInputLabel.style.flexDirection = 'column';
 
-const idIntervalLabel = document.createElement('label');
-idIntervalLabel.innerText = 'Interval in ms'
-const intervalInput = document.createElement('input');
-intervalInput.type = 'number';
-intervalInput.value = 5000;
+const idIntervalLabel = make('label', 'Interval in ms');
+const intervalInput = make('input', null, 5000, 'number');
 idIntervalLabel.appendChild(intervalInput);
-idIntervalLabel.style.display = 'flex';
-idIntervalLabel.style.flexDirection = 'column';
 
 const search = () => {
   let id = idInput.value;
-  let prefix = prefixInput.value;
-  let formular = document.querySelector('app-daf-input form');
+  const prefix = prefixInput.value;
+  const formular = document.querySelector('app-daf-input form');
 	const input = document.querySelector('app-daf-input [id^=mat-input-]');
 	input.value = `${prefix}${id}`;
-	const changeEvent = document.createEvent("HTMLEvents");
-  changeEvent.initEvent("change", false, true);
-	input.dispatchEvent(changeEvent);
-	const inputEvent = document.createEvent("HTMLEvents");
-  inputEvent.initEvent("input", false, true);
-	input.dispatchEvent(inputEvent);
+	
+	const newEvent = document.createEvent("HTMLEvents");
+  newEvent.initEvent("change", false, true);
+	input.dispatchEvent(newEvent);
+  newEvent.initEvent("input", false, true);
+	input.dispatchEvent(newEvent);
+	
 	id++;
 	localStorage.setItem('last_id', id);
 	idInput.value = id;
 	document.querySelector('.vin-search-button').click();
 }
 
-let searchInterval;
-
-const startSearchInterval = () => {
-  search();
-  searchInterval = setInterval(() => {
-    search()
-  }, intervalInput.value || 5000)
-}
-
-const stopSearchInterval = () => {
-  clearInterval(searchInterval);
-}
-
-let searchInProgress = false;
-
-const button = document.createElement('button');
-button.style.padding = '.5rem 1rem';
-button.style.backgroundColor = 'dodgerblue';
-button.style.color = 'white';
-button.style.border = 'none';
-button.style.borderRadius = '5px';
-button.style.marginTop = '10px';
-button.innerText = 'Start';
+const button = make('button', 'Start');
 button.onclick = () => {
   if (searchInProgress) {
-    stopSearchInterval();
+    clearInterval(searchInterval);
     searchInProgress = false;
     button.innerText = 'Start';
     button.style.backgroundColor = 'dodgerblue';
   } else {
-    startSearchInterval();
+    search();
+    searchInterval = setInterval(() => search(), intervalInput.value || 5000);
     searchInProgress = true;
     button.innerText = 'Stop';
     button.style.backgroundColor = 'firebrick';
   }
 };
 
-const modal = document.createElement('div');
-modal.id = 'auto-id-counter';
-// modal.style.all = 'unset';
-modal.style.position = 'fixed';
-modal.style.top = '1rem';
-modal.style.right = '1rem';
-modal.style.zIndex = 2000;
-modal.style.backgroundColor = 'aliceblue';
-modal.style.display = 'flex';
-modal.style.flexDirection = 'column';
-modal.style.padding = '1rem';
-modal.style.border = '1px solid dodgerblue';
-modal.style.borderRadius = '5px';
-modal.style.boxShadow = '10px 10px 10px -5px rgba(0,0,0,0.2)';
+const credit = make('span', `made with ❤ by <a href="https://stkng.de" target="_blank">Steffen</a>`);
+const styles = `
+  #auto-id-counter {
+    position: fixed;
+    top: 1rem;
+    right: 1rem;
+    z-index: 2000;
+    background: aliceblue;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    border: 1px solid dodgerblue;
+    border-radius: 5px;
+    boy-shadow: 10px 10px 10px -5px rgba(0,0,0,0.2);
+  }
+  
+  #auto-id-counter label {
+    display: flex;
+    flex-direction: column;
+    font-size: 10px;
+  }
+  
+  #auto-id-counter input {
+    font-size: 1rem;
+  }
+  
+  #auto-id-counter button {
+    padding: .5rem 1rem;
+    background-color: dodgerblue;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    margin: 10px 0;
+  }
+  
+  #auto-id-counter span {
+    font-size: 10px;
+    align-self: center;
+  }
+`
+const styleElement = make('style', styles);
 
+const modal = make('div', null, null, null, 'auto-id-counter');
+
+modal.appendChild(styleElement);
 modal.appendChild(prefixInputLabel);
 modal.appendChild(idInputLabel);
 modal.appendChild(idIntervalLabel);
 modal.appendChild(button);
+modal.appendChild(credit);
 document.body.appendChild(modal);
